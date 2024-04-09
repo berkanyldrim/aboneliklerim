5191e9115423a7b802c6860a87c8a2fe8be7ccda
berkanyldrim <berkanyldrim@gmail.com>
2024-04-09 22:35:37 +0300

    feat:user login form created

---
+++ client/src/views/components/UserLoginForm.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+// --- Import Vue ---
+import { ref } from "vue";
+
+// import LucideSpinner from '~icons/lucide/loader-2'
+// --- Import Utils ---
+import { cn } from "@/lib/utils";
+
+// --- Import Vee Validate ---
+import { useForm } from "vee-validate";
+
+// --- Import Schema ---
+import { loginSchema } from "@/form-schema/login";
+
+//NOTE - useForm
+const { handleSubmit } = useForm({
+  validationSchema: loginSchema,
+});
+
+//NOTE - Ref
+const isLoading = ref(false);
+
+//NOTE - onSubmit
+const onSubmit = handleSubmit(async (values) => {
+  isLoading.value = true;
+  console.log(values);
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 3000);
+});
+</script>
+
+<template>
+  <div :class="cn('grid gap-6', $attrs.class ?? '')">
+    <form @submit="onSubmit">
+      <div class="grid gap-2 mb-2">
+        <div class="grid gap-1">
+          <FormField v-slot="{ componentField }" name="email">
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  id="email"
+                  placeholder="name@example.com"
+                  type="email"
+                  auto-capitalize="none"
+                  auto-complete="email"
+                  auto-correct="off"
+                  :disabled="isLoading"
+                  v-bind="componentField"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+        </div>
+      </div>
+      <div class="grid gap-2 mb-2">
+        <div class="grid gap-1">
+          <FormField v-slot="{ componentField }" name="password">
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  id="password"
+                  placeholder="name@example.com"
+                  type="password"
+                  :disabled="isLoading"
+                  v-bind="componentField"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+        </div>
+      </div>
+
+      <div class="grid gap-2 mb-2">
+        <Button type="submit" :disabled="isLoading">
+          <!-- <LucideSpinner v-if="isLoading" class="mr-2 h-4 w-4 animate-spin" /> -->
+          Giriş Yap
+        </Button>
+      </div>
+      <div class="grid">
+        <p class="px-8 text-center text-sm text-muted-foreground">
+          Giriş yapamıyor musunuz?
+          <a
+            href="/terms"
+            class="underline underline-offset-4 hover:text-primary"
+          >
+            Şifremi Unuttum
+          </a>
+        </p>
+      </div>
+    </form>
+    <div class="relative">
+      <div class="absolute inset-0 flex items-center">
+        <span class="w-full border-t" />
+      </div>
+      <div class="relative flex justify-center text-xs">
+        <span class="bg-background px-2 text-muted-foreground">
+          Hesabın Yok mu?
+        </span>
+      </div>
+    </div>
+    <Button variant="outline" type="button" :disabled="isLoading">
+      <!-- <LucideSpinner v-if="isLoading" class="mr-2 h-4 w-4 animate-spin" /> -->
+
+      Hesap Oluştur
+    </Button>
+  </div>
+</template>
