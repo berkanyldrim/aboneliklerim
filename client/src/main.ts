55806d60d0c0692dc3f706d3e8dcea6bfac319ca
berkanyldrim <berkanyldrim@gmail.com>
2024-04-16 18:02:34 +0300

    chore:avatar dropdown tabs components added

--- client/src/main.ts
+++ client/src/main.ts
@@ -25,10 +25,25 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuGroup
+} from '@/components/ui/dropdown-menu'
+
 const app = createApp(App);
 
 app.use(router);
 
+app.component("Avatar", Avatar);
+app.component("AvatarFallback", AvatarFallback);
+app.component("AvatarImage", AvatarImage);
 app.component("Button", Button);
 app.component("Label", Label);
 app.component("Input", Input);
@@ -44,5 +59,16 @@ app.component("CardDescription", CardDescription);
 app.component("CardFooter", CardFooter);
 app.component("CardHeader", CardHeader);
 app.component("CardTitle", CardTitle);
+app.component("Tabs", Tabs);
+app.component("TabsContent", TabsContent);
+app.component("TabsList", TabsList);
+app.component("TabsTrigger", TabsTrigger);
+app.component("DropdownMenu", DropdownMenu);
+app.component("DropdownMenuContent", DropdownMenuContent);
+app.component("DropdownMenuItem", DropdownMenuItem);
+app.component("DropdownMenuLabel", DropdownMenuLabel);
+app.component("DropdownMenuSeparator", DropdownMenuSeparator);
+app.component("DropdownMenuTrigger", DropdownMenuTrigger);
+app.component("DropdownMenuGroup", DropdownMenuGroup);
 
 app.mount("#app");
