1581c1ef27de4039d41288c3221b7b06b584b8c7
berkanyldrim <berkanyldrim@gmail.com>
2024-04-18 13:50:58 +0300

    chore:side bar added

--- client/src/layout/AppLayout.vue
+++ client/src/layout/AppLayout.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { cn } from '@/lib/utils'
 import AppTopbar from './AppTopbar.vue';
+import AppSidebar from './AppSidebar.vue';
 </script>
 
 <template>
-    <div>
+    <AppSidebar />
+
+    <div :class="cn(`h-full w-fulloverflow-auto md:ml-64`)">
         <AppTopbar />
-        <router-view></router-view>
+        <div class="p-2 space-y-2">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
