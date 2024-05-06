7148315589add1eec9647b768b3bfc020f427a67
berkanyldrim <berkanyldrim@gmail.com>
2024-05-06 16:59:25 +0300

    chore:currency and migrate route added

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "../deps.ts";
 import { getUsers, createUser } from "../controllers/userController.ts";
+import { migrate } from "../migrations/migrate.ts";
+import { getCurrencies } from "../controllers/currenciesController.ts";
 
 const router = new Router();
 
@@ -7,7 +9,14 @@ router.get("/", (ctx) => {
   ctx.response.body = "Hello World!";
 });
 
+//NOTE - Migrastion routes
+ router.get("/mig", migrate);
+
+//NOTE - Users routes
 router.get("/users", getUsers);
 router.post("/users", createUser);
+
+//NOTE - Currencies routes
+router.get("/currencies", getCurrencies);
 
 export default router;
