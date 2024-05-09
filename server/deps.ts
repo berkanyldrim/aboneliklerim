13c413ab20405dad9da811b04c7db684a9f06e0b
berkanyldrim <berkanyldrim@gmail.com>
2024-05-09 10:38:18 +0300

    chore: jwt and bcrypt added

--- server/deps.ts
+++ server/deps.ts
@@ -1,7 +1,27 @@
+// --- Import Oak ---
 import { Application, Router,Context } from "https://deno.land/x/oak/mod.ts";
 
+// --- Import MongoDB ---
 import {
     MongoClient,
 } from "https://deno.land/x/mongo/mod.ts";
 
-export { Application, Router,  MongoClient,Context};
+// --- Import Djwt Validate ---
+import {
+  validateJwt,
+  parseAndDecode,
+  validateJwtObject,
+} from "https://deno.land/x/djwt/validate.ts";
+
+// --- Import Djwt ---
+import {
+  makeJwt,
+  setExpiration,
+  Jose,
+  Payload,
+} from "https://deno.land/x/djwt/create.ts";
+
+// --- Import Bcrypt ---
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
+
+export { Application, Router,  MongoClient,Context , validateJwt, parseAndDecode, validateJwtObject, makeJwt, setExpiration, Jose, Payload , bcrypt};
