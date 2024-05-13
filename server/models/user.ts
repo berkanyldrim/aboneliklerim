11fbcb98827577de56f6d25f8bff32e77d6fec91
berkanyldrim <berkanyldrim@gmail.com>
2024-05-13 16:23:34 +0300

    chore:user model complated

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,8 +1,13 @@
-export interface User {
-  _id: string;
+import db from '../connection/mongoDB.ts';
+interface UserSchema {
+  _id: { $oid: string};
   name: string;
   surname: string;
   email: string;
   password: string;
   // subscription: string[];
 }
+
+const users= db.collection<UserSchema>("users");
+
+export default users;
