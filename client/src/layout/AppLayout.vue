91e75490aff7daa7c429cbba28f3acdb70d7d95b
berkanyldrim <berkanyldrim@gmail.com>
2024-05-21 20:56:44 +0300

    chore:top bar deleted

--- client/src/layout/AppLayout.vue
+++ client/src/layout/AppLayout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
-import AppTopbar from './AppTopbar.vue';
+// import AppTopbar from './AppTopbar.vue';
 import AppSidebar from './AppSidebar.vue';
 </script>
 
@@ -8,8 +8,8 @@ import AppSidebar from './AppSidebar.vue';
     <AppSidebar />
 
     <div :class="cn(`h-full w-fulloverflow-auto md:ml-64`)">
-        <AppTopbar />
-        <div class="p-2 space-y-2">
+        <!-- <AppTopbar /> -->
+        <div class="p-2 space-y-2 mt-10">
             <router-view></router-view>
         </div>
     </div>
