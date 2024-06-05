21de0d979767c141d545156709511eab5966eee9
berkanyldrim <berkanyldrim@gmail.com>
2024-06-05 17:56:40 +0300

    chore:token added but not complated

--- client/composables/useMyFetch.js
+++ client/composables/useMyFetch.js
@@ -1,5 +1,19 @@
 export const useMyFetch = (request, opts) => {
   const config = useRuntimeConfig();
+  const token = localStorage.getItem("token");
 
-  return useFetch(request, { baseURL: config.public.baseURL, ...opts });
+  return useFetch(request, {
+    baseURL: config.public.baseURL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      ...opts?.headers,
+    },
+    ...opts,
+  }).then((response) => {
+    if (response.status === 401) {
+      // Eğer token geçersizse login sayfasına yönlendirme
+      navigateTo("/access");
+    }
+    return response;
+  });
 };
